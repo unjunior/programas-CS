0d9a6be761235b351a444eb95362d1d0d1fee76c
Ubirajara Junior <unjunior4315@gmail.com>
2023-02-18 21:31:10 -0300

    Implementada a classe FuncionarioTerceirizado.

--- funcionarios/PagamentosFunc/entidades/FuncionarioTerceirizado.cs
+++ funcionarios/PagamentosFunc/entidades/FuncionarioTerceirizado.cs
@@ -1,7 +1,21 @@
 namespace PagamentosFunc.entidades
 {
-    class FuncionarioTerceirizado
+    class FuncionarioTerceirizado : Funcionario
     {
-        
+        public double DespesaAdicional { get; set; }
+
+        public FuncionarioTerceirizado()
+        {
+
+        }
+        public FuncionarioTerceirizado(string nome, int horas, double valorPorHora, double despesaAdicional)
+            :base(nome, horas, valorPorHora)
+        {
+            DespesaAdicional = despesaAdicional;
+        }
+        public override double Pagamento()
+        {
+            return base.Pagamento() + 1.1 * DespesaAdicional;
+        }
     }
 }
