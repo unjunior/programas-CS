4b7d24575bc79086137ba109e3cbe9e95d3f5711
Ubirajara Junior <unjunior4315@gmail.com>
2023-02-18 21:18:41 -0300

    Implementada a classe Funcionario

--- funcionarios/PagamentosFunc/entidades/Funcionario.cs
+++ funcionarios/PagamentosFunc/entidades/Funcionario.cs
@@ -2,6 +2,25 @@ namespace PagamentosFunc.entidades
 {
     class Funcionario
     {
-        
+        public string Nome { get; set; }
+        public int Horas { get; set; }
+        public double ValorPorHora { get; set; }
+
+        public Funcionario()
+        {
+        }
+        public Funcionario(string nome, int horas, double valorPorHora){
+            
+            Nome = nome;
+            Horas = horas;
+            ValorPorHora = valorPorHora;
+        }
+
+        public virtual double Pagamento()
+        {
+            return Horas * ValorPorHora;
+        }
     }
+        
+    
 }
